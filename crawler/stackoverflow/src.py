14c4e95bed70c8d9bac1a93d21c3433ba9443aac
zsq259 <2530059525@qq.com>
2023-07-15 19:36:32 +0800

    feat: crawl detail links in stackoverflow

--- crawler/stackoverflow/src.py
+++ crawler/stackoverflow/src.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from playwright.sync_api import Playwright, sync_playwright, expect
-
+from keywords import keywords
+import time, threading
 
 def run(playwright: Playwright, o) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://stackoverflow.com/")
     page.get_by_placeholder("Search…").click()
     page.get_by_placeholder("Search…").fill(o)
     page.get_by_placeholder("Search…").press("Enter")
-    show = page.selector("xpath=//a[@title='Show 50 items per page']")
-    print(show)
+    time.sleep(0.2)
+    show = page.query_selector("xpath=//a[@title='Show 50 items per page']")
     if show != None: show.click()
-
+    cnt = 0
+    while (True):
+        time.sleep(1)
+        elements = page.query_selector_all("xpath=//h3[@class='s-post-summary--content-title']/a")
+        for element in elements:
+            with open("links/{}.out".format(o), 'a') as f:
+                f.write(element.get_attribute('href') + '\n')
+        cnt += 1
+        next = page.query_selector("xpath=//a[@rel='next']")
+        if next == None: break
+        next.click()
+    print(o, "total page:", cnt, "get!")
 
     page.close()
 
@@ -25,6 +37,6 @@ def run(playwright: Playwright, o) -> None:
 
 def work(o):
     with sync_playwright() as playwright:
-        run(playwright)
+        run(playwright, o)
 
-work("primary")
+for o in keywords: work(o)
